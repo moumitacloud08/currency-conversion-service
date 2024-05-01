b00e3cc77d2cd53532f155b47c64d5e7a9a56da5
moumitacloud08 <moumitacloud08@gmail.com>
2024-05-01 11:59:51 -0700

    adding rest template so that it can show up in zipking tracing before it was not showing because it was calling new RestTemplate()
    restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
    CurrencyConversionBean.class,urivariables);

--- src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -5,17 +5,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+    
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
+
 @RestController
 public class CurrencyConversionController {
 	
 	@Autowired
 	CurrencyExchangeServiceProxy proxy;
+	
+	@Autowired
+    private RestTemplate restTemplate;
+
 	
 	
 	@GetMapping("/currency-converter"
@@ -25,7 +41,13 @@ public class CurrencyConversionController {
 		Map<String,String> urivariables = new HashMap<>();
 		urivariables.put("from", from);
 		urivariables.put("to", to);
-		ResponseEntity<CurrencyConversionBean> responseEntity=  new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,urivariables);
+		/**
+		 * adding rest template so that it can show up in zipking tracing
+		 * before it was not showing because it was calling new RestTemplate()
+		 */
+		//ResponseEntity<CurrencyConversionBean> responseEntity=  new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,urivariables);
+		ResponseEntity<CurrencyConversionBean> responseEntity= restTemplate.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,urivariables);
+
 		CurrencyConversionBean response = responseEntity.getBody();
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
 
